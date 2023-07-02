c8302f7242bc8eb0d8075dc02ec02fc934643774
InfoCQandP <134182813+InfoCQandP@users.noreply.github.com>
2023-07-02 16:05:05 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function mostrarMensaje() {
+    alert("Mensaje enviado correctamente");
+
+      // Limpiar los campos de entrada de texto
+      location.reload();
+  }
+  
+  function mostrarMensaje(preescolar) {
+    alert("Sólo es un ejemplo " + preescolar);
+    location.reload();
+  }
+
+  function mostrarMensaje(primaria) {
+    alert("Sólo es un ejemplo " + primaria);
+    location.reload();
+  }
+
+  function mostrarMensaje(secundaria) {
+    alert("Sólo es un ejemplo " + secundaria);
+    location.reload();
+  }
+
+  function mostrarMensaje(preparatoria) {
+    alert("Sólo es un ejemplo " + preparatoria);
+    location.reload();
+  }
